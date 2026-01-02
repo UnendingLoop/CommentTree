39d42b70d5700d13533f97fecb80d41377e6bf56
FANIL KAMA <1618396fkam@gmail.com>
2026-01-02 13:54:37 +0000

    Added graceful shutdown

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,7 +2,13 @@
 package cmd
 
 import (
+	"context"
+	"errors"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"commentTree/internal/api"
@@ -10,6 +16,7 @@ import (
 	"commentTree/internal/service"
 
 	"github.com/wb-go/wbf/config"
+	"github.com/wb-go/wbf/dbpg"
 	"github.com/wb-go/wbf/ginext"
 )
 
@@ -23,11 +30,6 @@ func StartApp() {
 
 	// Connecting to database
 	dbConn := repository.ConnectWithRetries(appConfig, 5, 10*time.Second)
-	defer func() {
-		if err := dbConn.Master.Close(); err != nil {
-			log.Println("Failed to close DB-conn correctly:", err)
-		}
-	}()
 
 	// Creating Repository
 	repo := repository.NewPostgresRepo(dbConn)
@@ -42,17 +44,61 @@ func StartApp() {
 	handlers := api.NewCommentHandlers(svc)
 
 	// Configuring server
-	server := ginext.New("") // empty - debug mode, release - prod mode
-	server.GET("/ping", handlers.SimplePinger)
-	server.POST("/comments", handlers.Create)                    // создание (с указанием родительского)
-	server.GET("/comments", handlers.GetAllRootComments)         // получение всех корневых комментариев с ?page=1&limit=20&sort=created_at
-	server.GET("/comments/:id", handlers.GetCommentWithChildren) // получение коммента с id и всех его детей
-	server.DELETE("/comments/:id", handlers.DeleteComment)       // удаление комментария и всех вложенных под ним
-	server.GET("/comments/search", handlers.RunSearch)           // поиск
-	server.Static("/web", "./internal/web")
+	mode := appConfig.GetString("GIN_MODE")
+	engine := ginext.New(mode)
+
+	engine.GET("/ping", handlers.SimplePinger)
+	engine.POST("/comments", handlers.Create)                    // создание (с указанием родительского)
+	engine.GET("/comments", handlers.GetAllRootComments)         // получение всех корневых комментариев с поддержкой квери ?page=1&limit=20&sort=created_at&order=ascending
+	engine.GET("/comments/:id", handlers.GetCommentWithChildren) // получение коммента с id и всех его детей
+	engine.DELETE("/comments/:id", handlers.DeleteComment)       // удаление комментария и всех вложенных под ним
+	engine.GET("/comments/search", handlers.RunSearch)           // поиск
+	engine.Static("/web", "./internal/web")
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: engine,
+	}
 
 	// Server launch
-	if err := server.Run(":8080"); err != nil {
-		log.Fatal(err)
+	go func() {
+		log.Printf("Server running on http://localhost%s\n", srv.Addr)
+		err := srv.ListenAndServe()
+		if err != nil {
+			switch {
+			case errors.Is(err, http.ErrServerClosed):
+				log.Println("Server gracefully stopping...")
+			default:
+				log.Fatalf("Server stopped: %v", err)
+			}
+		}
+	}()
+
+	// Waiting for interruption to start Graceful shutdown
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
+
+	shutdown(srv, dbConn)
+	log.Println("Exiting application...")
+}
+
+func shutdown(srv *http.Server, dbConn *dbpg.DB) {
+	log.Println("Interrupt received!!! Starting shutdown sequence...")
+
+	// 5 seconds to stop HTTP-server:
+	ctx, httpCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer httpCancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("Server shutdown error: %v", err)
+	} else {
+		log.Println("HTTP server stopped")
 	}
+
+	// Closing DB connection
+	if err := dbConn.Master.Close(); err != nil {
+		log.Println("Failed to close DB-conn correctly:", err)
+		return
+	}
+	log.Println("DBconn closed")
 }
